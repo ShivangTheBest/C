937f7cf42545631b439b1191c73f671a85caa8a6
Shivang <95877731+ShivangTheBest@users.noreply.github.com>
2025-01-08 22:37:25 +0530

    20_pointers.c

--- 20_pointers.c
+++ 20_pointers.c
@@ -4,12 +4,14 @@
 
 int main()
 {
-    int i = 72;
-    printf("The address of i is: \n%p\n", &i);
-    printf("The address of i is: \n%u\n", &i);   //%u stands for unsigned integer.
-
-    int* j = &i;   //j is the address of i.
-    printf("The address of i is:\n%d", j);
+    int i = 72;       // Declare an integer variable i and initialize it to 72
+    int *j = &i;      // Declare a pointer variable j and initialize it to the address of i
+    
+    printf("The value of i is %d\n", i);     // Output the value of i (72)
+    printf("The value of i is %d\n", *j);    // Output the value at the address stored in j (72)
+    printf("The address of i is %u\n", &i);   // Output the address of i
+    printf("The address stored in j is %u\n", j);     // Output the address stored in j (same as the address of i)
+    
     return 0;
 }
 // * puchta hai kya value hai, aur & puchta hai kya address hai.
@@ -20,10 +22,10 @@ int main()
 int main()
 {
     char i = 'A';
-    char* j = &i;
+    char *j = &i;
 
     float k = 555;
-    float* l = &k;
+    float *l = &k;
     return 0;
 }
 // Note that the format of address must be the same as format of the value stored.
